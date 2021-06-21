218bb89753c687443cbb5c92908b38997bc2c662
Gorkemblm <gorkemblm@gmail.com>
2021-06-21 03:31:59 +0300

    general code refactor

--- src/pages/CurriculumVitaeList.js
+++ src/pages/CurriculumVitaeList.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import CurriculumVitaeService from '../services/curriculumVitaeService'
-import { Table, Dropdown, Select } from 'semantic-ui-react'
+import { Table, Dropdown } from 'semantic-ui-react'
 
 export default function CurriculumVitaeList() {
 
@@ -32,11 +32,6 @@ export default function CurriculumVitaeList() {
                                 <Table.Cell>{curriculumVitae.jobSeeker.firstName}</Table.Cell>
                                 <Table.Cell>{curriculumVitae.jobSeeker.lastName}</Table.Cell>
                                 <Table.Cell><Dropdown placeholder='Okul' search selection /></Table.Cell>
-                                {
-                                    curriculumVitae.schools.map((school) => (
-                                              <Select>dsds</Select>
-                                    ))
-                                }
                                 <Table.Cell><Dropdown placeholder='Dil' search selection /></Table.Cell>
                                 <Table.Cell><Dropdown placeholder='Yetenek' search selection /></Table.Cell>
                                 <Table.Cell><Dropdown placeholder='Deneyim' search selection /></Table.Cell>
--- src/pages/JobAdvertisementList.js
+++ src/pages/JobAdvertisementList.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import '../App.css';
 import '../pages/jobAdvertisement.css'
-import { Item, Label, Container, Icon } from 'semantic-ui-react'
+import { Item, Label, Container, Icon, Grid } from 'semantic-ui-react'
 import { Pagination } from 'semantic-ui-react'
 import JobAdvertisementService from '../services/jobAdvertisementService';
 
@@ -17,50 +17,57 @@ export default function JobAdvertisementList() {
 
 
 
-  return <div className='jobAdvertisements'>
+  return (
+    <div className='jobAdvertisements'>
+      <Container>
+        <Grid >
+          <Grid.Column className='jobAdvertisement'>
+            <Item.Group divided>
+              {
+                jobAdvertisements.map((jobAdvertisement) => (
+                  <Item key={jobAdvertisement.id}>
+                    <Item.Image className='corporateImage' size='small' src='https://res.cloudinary.com/gorkemblm/image/upload/v1623418004/file_lul3m6.jpg' />
+                    <Item.Content>
+                      <div>
+                        <Item.Header><h1 className='companyName'>{jobAdvertisement.employer.companyName}</h1></Item.Header><br />
+                      </div>
 
-    <Container className='jobAdvertisement'>
-      <Item.Group divided>
-        {
-          jobAdvertisements.map((jobAdvertisement) => (
-            <Item key={jobAdvertisement.id}>
-              <Item.Image className='corporateImage' size='small' src='https://res.cloudinary.com/gorkemblm/image/upload/v1623418004/file_lul3m6.jpg' />
-              <Item.Content>
-                <div>
-                  <Item.Header><h1 className='companyName'>{jobAdvertisement.employer.companyName}</h1></Item.Header><br />
-                </div>
+                      <Item.Header as='a'><h3 className='occupationName'>{jobAdvertisement.occupation.occupationName}</h3></Item.Header>
+                      <Item.Meta>
+                        <span>Tam Zamanlı</span> / <span>Remote</span>
+                      </Item.Meta>
+                      <Item.Description>
+                        {jobAdvertisement.jobDescription}
+                      </Item.Description>
+                      <Item.Extra>
+                        <Label>
+                          <Icon name='map marker' />{jobAdvertisement.city.cityName}</Label><br />
+                        <Label>Spring Boot</Label>
+                        <Label>Java</Label>
+                        <Label>SOLID</Label>
+                        <Label>İngilizce</Label><br />
+                        <Label as='a' basic color='red' >Son Başvuru Tarihi : {jobAdvertisement.applicationDeadline}</Label>
+                        <Label as='a' basic color='green'>Yayınlanma Tarihi : {jobAdvertisement.updatedAt}</Label><br />
+                        <Label as='a' basic color='black'>
+                          Başvuru Sayısı
+                          <Label.Detail>{jobAdvertisement.numberOfApplication}</Label.Detail>
+                        </Label>
+                      </Item.Extra>
+                    </Item.Content>
+                  </Item>
+                ))}
+            </Item.Group>
+          </Grid.Column>
+        </Grid>
 
-                <Item.Header as='a'><h3 className='occupationName'>{jobAdvertisement.occupation.occupationName}</h3></Item.Header>
-                <Item.Meta>
-                  <span>Tam Zamanlı</span> / <span>Remote</span>
-                </Item.Meta>
-                <Item.Description>
-                  {jobAdvertisement.jobDescription}
-                </Item.Description>
-                <Item.Extra>
-                  <Label>
-                    <Icon name='map marker' />{jobAdvertisement.city.cityName}</Label><br />
-                  <Label>Spring Boot</Label>
-                  <Label>Java</Label>
-                  <Label>SOLID</Label>
-                  <Label>İngilizce</Label><br />
-                  <Label as='a' basic color='red' >Son Başvuru Tarihi : {jobAdvertisement.applicationDeadline}</Label>
-                  <Label as='a' basic color='green'>Yayınlanma Tarihi : {jobAdvertisement.updatedDate}</Label><br />
-                  <Label as='a' basic color='black'>
-                    Başvuru Sayısı
-                    <Label.Detail>{jobAdvertisement.numberOfApplication}</Label.Detail>
-                  </Label>
-                </Item.Extra>
-              </Item.Content>
-            </Item>
-          ))}
-      </Item.Group>
-    </Container>
-    <Container>
-      <div className='pagination'>
-        <Pagination defaultActivePage={3} totalPages={10} />
-      </div>
-    </Container>
-  </div>;
+        <Container>
+          <div className='pagination'>
+            <Pagination defaultActivePage={3} totalPages={10} />
+          </div>
+        </Container>
+      </Container>
+
+    </div>
+  )
 
 }
