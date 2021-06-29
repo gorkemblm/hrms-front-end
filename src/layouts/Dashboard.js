3977b671ebe404750618e6985fc253d9064ed743
Görkem Bilim <83100550+gorkemblm@users.noreply.github.com>
2021-06-30 02:25:31 +0300

    General code refactor (#14)
    * deleted useless components
    
    * code refactor for deleted components

--- src/layouts/Dashboard.js
+++ src/layouts/Dashboard.js
@@ -1,18 +1,11 @@
 import React from 'react'
 import { Route } from 'react-router'
 import JobAdvertisementList from '../pages/JobAdvertisementList'
-import EmployerList from '../pages/EmployerList'
-import OccupationList from '../pages/OccupationList'
-import JobSeekerList from '../pages/JobSeekerList'
-import CurriculumVitaeList from '../pages/CurriculumVitaeList'
 import Home from '../pages/Home'
 import SignIn from '../pages/SignIn'
 import JobSeekerSignUp from '../pages/JobSeekerSignUp'
 import EmployerSignUp from '../pages/EmployerSignUp'
 import { Container, Grid } from 'semantic-ui-react'
-import OccupationDetail from '../pages/OccupationDetail'
-import EmployerDetail from '../pages/EmployerDetail'
-import JobSeekerDetail from '../pages/JobSeekerDetail'
 import JobSeekerProfile from '../pages/JobSeekerProfile'
 import EmployerProfile from '../pages/EmployerProfile'
 
@@ -27,22 +20,14 @@ export default function Dashboard() {
                         </Grid.Column>
                         <Grid.Column width={12}>
                             <Route exact path='/' component={Home} />
-                            
                         </Grid.Column>
                     </Grid.Row>
                 </Grid>
                 <Route path='/jobAdvertisements' component={JobAdvertisementList} />
-                <Route path='/employers' component={EmployerList} />
-                <Route path='/jobSeekers' component={JobSeekerList} />
-                <Route path='/occupations' component={OccupationList} />
-                <Route path='/curriculumvitaes' component={CurriculumVitaeList} />
                 <Route exact path='/sign-in' component={SignIn} />
                 <Route path='/sign-up-job-seeker' component={JobSeekerSignUp} />
                 <Route path='/sign-up-employer' component={EmployerSignUp} />
-                <Route path='/occupation/:id' component={OccupationDetail} />
-                <Route path='/employer/:id' component={EmployerDetail} />
-                <Route path='/jobSeeker/:id' component={JobSeekerDetail} />
-                <Route path='/jobseeker' component={JobSeekerProfile} />
+                <Route exact path='/jobseeker' component={JobSeekerProfile} />
                 <Route path='/employer' component={EmployerProfile} />
             </Container>
         </div>
--- src/pages/JobSeekerProfile.js
+++ src/pages/JobSeekerProfile.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Menu, Grid, Icon, Label, Image} from 'semantic-ui-react'
+import { Route } from 'react-router'
+import { Menu, Grid, Icon, Label, Image } from 'semantic-ui-react'
 import CurriculumVitaeAdd from './CurriculumVitaeAdd'
 
 export default function JobSeekerProfile() {
 
-    
+
     return (
         <div>
             <Grid>
@@ -15,12 +16,17 @@ export default function JobSeekerProfile() {
                                 <Image circular spaced="right" src="https://res.cloudinary.com/gorkemblm/image/upload/v1623418067/file_oslkvg.jpg" />
                             </Menu.Item>
                             <Menu.Item
+                                name='photo'
+                            >
+                                Fotoğraf Değiştir
+                                <Icon name='edit'></Icon>
+                            </Menu.Item>
+                            <Menu.Item
                                 name='messages'
                             >
-                                Bildirimler
-                                <Label color='red'><Icon name='filter'></Icon>12</Label>
+                                Cv Oluştur
+                                <Icon name='paperclip'></Icon> 
                             </Menu.Item>
-
                             <Menu.Item
                                 name='messages'
                             >
@@ -30,7 +36,8 @@ export default function JobSeekerProfile() {
                         </Menu>
                     </Grid.Column>
                     <Grid.Column width={12}>
-                        {/* <CurriculumVitaeAdd/> */}
+                        {/* <Route  path='/curriculumVitaeAdd' component={CurriculumVitaeAdd} /> */}
+                        <CurriculumVitaeAdd />
                     </Grid.Column>
                 </Grid.Row>
             </Grid>
