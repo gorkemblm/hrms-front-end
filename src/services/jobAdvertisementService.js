6d7cb0d67c768e5cae96a18997f0d1410ffe13c4
Gorkemblm <gorkemblm@gmail.com>
2021-06-30 02:22:23 +0300

    refactor job advertisement service

--- src/services/jobAdvertisementService.js
+++ src/services/jobAdvertisementService.js
@@ -4,4 +4,20 @@ export default class JobAdvertisementService {
     getJobAdvertisements() {
         return axios.get('http://localhost:8080/api/jobAdvertisements/getall')
     }
+
+    getCities() {
+        return axios.get("http://localhost:8080/api/cities/getAll")
+    }
+
+    getWayOfWorkings() {
+        return axios.get("http://localhost:8080/api/wayOfWorkings/getAll")
+    }
+
+    getWorkingTimes() {
+        return axios.get("http://localhost:8080/api/workingTimes/getAll")
+    }
+
+    getOccupations() {
+        return axios.get("http://localhost:8080/api/occupations/getall")
+    }
 }
